2af305df0bcef9b9565855dc52f98cac1081fd6b
wgsxm <2901199020@qq.com>
2023-05-09 20:25:10 +0800

    update gitignore

--- value.cpp
+++ value.cpp
@@ -1,12 +1,10 @@
 #include "./value.h"
-
 #include <cmath>
 #include <iomanip>
 BooleanValue::BooleanValue(bool value) : m_value(value) {}
 std::string BooleanValue::toString() const {
     return m_value ? "#t" : "#f";
 }
-
 NumericValue::NumericValue(double value) : m_value(value) {}
 std::string NumericValue::toString() const  {
     if (std::floor(m_value) == m_value) {
@@ -15,21 +13,17 @@ std::string NumericValue::toString() const  {
         return std::to_string(m_value);
     }
 }
-
 StringValue::StringValue(const std::string& value) : m_value(value) {}
 std::string StringValue::toString() const  {
     return std::string("\"") + m_value + std::string("\"");
 }
-
 std::string NilValue::toString() const {
     return std::string("()");
 }
-
 SymbolValue::SymbolValue(const std::string& name) : m_name(name) {}
 std::string SymbolValue::toString() const  {
     return m_name;
 }
-
 PairValue::PairValue(std::shared_ptr<Value> left, std::shared_ptr<Value> right)
     : m_left(left), m_right(right) {}
 std::string PairValue::toString() const {
--- value.h
+++ value.h
@@ -1,57 +1,45 @@
 #ifndef VALUE_H
 #define VALUE_H
-
 #include <memory>
 #include <string>
-
 class Value {
 public:
     virtual ~Value() = default;
     virtual std::string toString() const = 0;
 };
-
 class BooleanValue : public Value {
 public:
     BooleanValue(bool value);
     std::string toString() const override;
-
 private:
     const bool m_value;
 };
-
 class NumericValue : public Value {
 public:
     NumericValue(double value);
     std::string toString() const override;
-
 private:
     const double m_value;
 };
-
 class StringValue : public Value {
 public:
     StringValue(const std::string& value);
     std::string toString() const override;
-
 private:
     const std::string m_value;
 };
-
 class NilValue : public Value {
 public:
     NilValue() = default;
     std::string toString() const override;
 };
-
 class SymbolValue : public Value {
 public:
     SymbolValue(const std::string& name);
     std::string toString() const override;
-
 private:
     const std::string m_name;
 };
-
 class PairValue : public Value {
 public:
     PairValue(std::shared_ptr<Value> left, std::shared_ptr<Value> right);
@@ -59,5 +47,4 @@ public:
     std::shared_ptr<Value> m_left;
     std::shared_ptr<Value> m_right;
 };
-
 #endif
