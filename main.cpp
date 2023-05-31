92a1c43b3faf9eec41573d8680e15c507a0c2182
wgsxm <2901199020@qq.com>
2023-06-01 01:07:25 +0800

    FINISH ALL!!!

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "./tokenizer.h"
 #include"./value.h"
 #include"./parser.h"
@@ -17,8 +18,30 @@ struct TestCtx {
     }
 };
 int main() {
- RJSJ_TEST(TestCtx, Lv2, Lv3,Lv4,Lv5,Lv5Extra,Lv6,Lv7,Lv7Lib,Sicp);
+    RJSJ_TEST(TestCtx, Lv2, Lv3,Lv4,Lv5,Lv5Extra,Lv6,Lv7,Lv7Lib,Sicp);
     std::shared_ptr<EvalEnv> env = EvalEnv::createGlobal();
+
+    //std::string filePath = "lv7-answer.scm";  // 文件路径
+    //std::ifstream inputFile(filePath);        // 打开文件
+    //std::string line;
+    //if (inputFile.is_open()) {
+    //    // 逐行读取文件内容
+    //    while (std::getline(inputFile, line)) {
+    //        try {
+    //            // 处理读取到的每一行数据
+    //            auto tokens = Tokenizer::tokenize(line);
+    //            Parser parser(std::move(tokens));  // TokenPtr 不支持复制
+    //            auto value = parser.parse();
+    //            env->eval(std::move(value));
+    //        } catch (std::runtime_error& e) {
+    //            std::cerr << "Error: " << e.what() << std::endl;
+    //        }
+    //    }
+    //    inputFile.close();  // 关闭文件
+    //} else {
+    //    std::cerr << "Failed to open file: " << filePath << std::endl;
+    //}
+    
     while (true) {
         try {
             std::cout << ">>> " ;
@@ -32,9 +55,6 @@ int main() {
             auto value = parser.parse();
             auto result = env->eval(std::move(value));
             std::cout << result->toString() << std::endl;
-            /*for (auto& token : tokens) {
-                std::cout << *token << std::endl;
-            }*/
         } catch (std::runtime_error& e) {
             std::cerr << "Error: " << e.what() << std::endl;
         }
